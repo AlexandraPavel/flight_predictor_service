91615bef11d80c461027ffd3600f94190c257b3e
Mircea Timpuriu <mircea.timpuriu@stud.acs.upb.ro>
2024-01-21 16:46:28 +0200

    backend

--- backend/server/server/wsgi.py
+++ backend/server/server/wsgi.py
@@ -7,21 +7,34 @@ application = get_wsgi_application()
 import inspect
 from apps.ml.registry import MLRegistry
 from apps.ml.income_classifier.random_forest import RandomForestClassifier
+from apps.ml.income_classifier.flight_predictor_rfr import RandomForestRegression
 # from apps.ml.income_classifier.extra_trees import ExtraTreesClassifier # import ExtraTrees ML algorithm
 
 try:
     registry = MLRegistry() # create ML registry
     # Random Forest classifier
-    rf = RandomForestClassifier()
+    rf = RandomForestRegression()
     # add to ML registry
-    registry.add_algorithm(endpoint_name="income_classifier",
+    registry.add_algorithm(endpoint_name="flight",
                             algorithm_object=rf,
                             algorithm_name="random forest",
                             algorithm_status="production",
                             algorithm_version="0.0.1",
-                            owner="Piotr",
-                            algorithm_description="Random Forest with simple pre- and post-processing",
-                            algorithm_code=inspect.getsource(RandomForestClassifier))
+                            owner="Mircea",
+                            algorithm_description="Flight random forest",
+                            algorithm_code=inspect.getsource(RandomForestRegression))
+    #registry = MLRegistry() # create ML registry
+    # Random Forest classifier
+    #rf = RandomForestClassifier()
+    # add to ML registry
+    #registry.add_algorithm(endpoint_name="income_classifier",
+    #                        algorithm_object=rf,
+    #                        algorithm_name="random forest",
+    #                        algorithm_status="production",
+    #                        algorithm_version="0.0.1",
+    #                        owner="Piotr",
+    #                        algorithm_description="Random Forest with simple pre- and post-processing",
+    #                        algorithm_code=inspect.getsource(RandomForestClassifier))
 
     # Extra Trees classifier
     # et = ExtraTreesClassifier()
